f9d89e45e6761486ba37a0dd9b2034c7de5254a8
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-02 08:40:36 -0600

    Initial Commit

---
+++ LocationBackgroundDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationBackgroundDemo
+//
+//  Created by Marco Alonso on 02/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
